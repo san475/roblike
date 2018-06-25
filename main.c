ec7d0a1356c600b6a06b95e76293cbb097fe9da6
san475 <franj696@gmail.com>
2018-06-25 13:08:00 -0400

    refactored position checking, added some functionality, TODO, health and position sub-bar

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include <string.h>
 
 //Structure Definitions
 typedef struct Player
@@ -12,12 +13,11 @@ typedef struct Player
 } Player;
 
 //Function Declarations
-
-
 int screenSetUp();
 int mapSetUp();
 Player * playerSetUp();
 int handle(int input, Player * user);
+int checkPos(int dir, char tar, Player * user);
 int printPlayer(int y, int x, Player * userMove);
 
 //Main Function  
@@ -29,19 +29,17 @@ int main (void)
     mapSetUp();
 
     user = playerSetUp();
+
 //Main game loop
     while((ch = getch()) != 'y')
     {
-        
         handle(ch,user);
 
     }
 
     endwin();
-
     return 0;
 }
-
 
 //Player Set Up
 Player * playerSetUp()
@@ -50,7 +48,7 @@ Player * playerSetUp()
     newPlayer = malloc(sizeof(Player));
 
     newPlayer->xPosition = 14;
-    newPlayer->yPosition = 10;
+    newPlayer->yPosition = 14;
     newPlayer->health = 28;
     mvprintw(newPlayer->yPosition,newPlayer->xPosition, "@");
     move(newPlayer->yPosition, newPlayer->xPosition);
@@ -71,57 +69,81 @@ int screenSetUp()
 //Map Draw
 int mapSetUp()
 {
-    mvprintw(12, 13, "---------");
-    mvprintw(13, 13, "|.......|");
-    mvprintw(14, 13, "|.......|");
-    mvprintw(15, 13, "|.......|");
-    mvprintw(16, 13, "|.......|");
-    mvprintw(17, 13, "|.......|");
-    mvprintw(18, 13, "---------");
+    mvprintw(12, 13, "----------------");
+    mvprintw(13, 13, "|..............|");
+    mvprintw(14, 13, "|..............|");
+    mvprintw(15, 13, "|..............|");
+    mvprintw(16, 13, "|..............|");
+    mvprintw(17, 13, "|..............|");
+    mvprintw(18, 13, "----------------");
 }
 
 int handle(int input,Player * user){
-
-    
     switch(input)
     {
         //UP
         case 'w':
         case 'W':
-            if(mvinch(user->yPosition-1, user->xPosition) != '-')
+            if(checkPos(0, '-', user) != 1)
                 printPlayer(user->yPosition-1, user->xPosition, user);
-            move(user->yPosition, user->xPosition);
             break;
-            //LEFT
+            
+        //LEFT
         case 'a':
         case 'A':
-            if(mvinch(user->yPosition, user->xPosition - 1) != '|')
+            if(checkPos(3, '|', user) != 1)
                 printPlayer(user->yPosition, user->xPosition-1, user);
-            move(user->yPosition, user->xPosition);
             break;
-            //RIGHT
+        //RIGHT
         case 'd':
         case 'D':
-            if(mvinch(user->yPosition, user->xPosition + 1) != '|')
+            if(checkPos(1, '|', user) != 1)
                 printPlayer(user->yPosition, user->xPosition+1, user);
-            move(user->yPosition, user->xPosition);
             break;
-            //DOWN
+        //DOWN
         case 's':
         case 'S':
-            if(mvinch(user->yPosition+1, user->xPosition) != '-')
+            if(checkPos(2, '-', user) != 1)
                 printPlayer(user->yPosition+1, user->xPosition, user);
-            move(user->yPosition, user->xPosition);
             break;
 
         default:
             break;
-
     }
     return 0;
 }
 
+//Put in direction, 0 is up, going clockwise, 1 is right, etc. 'target', and the Player, and will return 1 if the target is found
+int checkPos(int dir, char tar, Player * user)
+{
+    int returning = 0;
+    if(dir == 0)
+    {
+        if(mvinch(user->yPosition - 1, user->xPosition) == tar)
+            returning = 1;
+    }
+    else if(dir == 1)
+    {
+        if(mvinch(user->yPosition, user->xPosition + 1) == tar)
+            returning = 1;
+    }
+    else if(dir == 2)
+    {
+        if(mvinch(user->yPosition + 1, user->xPosition) == tar)
+            returning = 1;
+    }
+    else if(dir == 3)
+    {
+        if(mvinch(user->yPosition, user->xPosition - 1) == tar)
+            returning = 1;
+    }
 
+    move(user->yPosition, user->xPosition);
+
+    return returning;
+} 
+
+//moves player to new position
 int printPlayer(int y, int x, Player * userMove){
 
     mvprintw(userMove->yPosition, userMove->xPosition, ".");
